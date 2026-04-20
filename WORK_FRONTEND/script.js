c74d314599d0cf9ef614e699bf1a25c1b6c9a4e3
RAMYA <151381036+SSURYA31@users.noreply.github.com>
2026-04-20 12:21:15 +0530

    xc

--- WORK_FRONTEND/script.js
+++ WORK_FRONTEND/script.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://127.0.0.1:8000";
+const BASE_URL = "https://python-developer-intern-task-submission.onrender.com";
 let token = localStorage.getItem("token") || "";
 let isLogin = true;
 
